2cc88fca22baf96bab0275e0bd5c149b7e57aa27
JingerTea <72108315+JingerTea@users.noreply.github.com>
2022-10-07 20:12:15 -0400

    Update convert_html.js

--- convert_html.js
+++ convert_html.js
@@ -1,3 +1,6 @@
+let html = "";
+let block_html, title, id, link;
+
 function convert(block_type) {
     let blocks;
     let text_html = "";
@@ -93,131 +96,131 @@ function convert(block_type) {
     return text_html;
 }
 
+function notion_to_html(items) {
+    items.forEach(element => {
+        let block = element.json;
+        let block_html = "";
 
-let html = "";
-let block_html, title, id, link;
+        switch (block.type) {
+            case 'paragraph':
+                text_html = convert(block.paragraph);
+                block_html = `<p>${text_html}</p>`;
+                break;
 
-items.forEach(element => {
-    let block = element.json;
-    let block_html = "";
+            case 'child_page':
+                title = block.child_page.title;
+                id = block.parent_id;
+                link = `https://www.notion.so/${id}`;
+                block_html = `<p><a href="${link}" >${title}</a></p>`;
+                break;
 
-    switch (block.type) {
-        case 'paragraph':
-            text_html = convert(block.paragraph);
-            block_html = `<p>${text_html}</p>`;
-            break;
+            case 'to_do':
+                text_html = convert(block.to_do);
+                block_html = `<ul><li>${text_html}</li></ul>`;
+                break
 
-        case 'child_page':
-            title = block.child_page.title;
-            id = block.parent_id;
-            link = `https://www.notion.so/${id}`;
-            block_html = `<p><a href="${link}" >${title}</a></p>`;
-            break;
+            case 'heading_1':
+                text_html = convert(block.heading_1);
+                block_html = `<h1>${text_html}</h1>`;
+                break;
 
-        case 'to_do':
-            text_html = convert(block.to_do);
-            block_html = `<ul><li>${text_html}</li></ul>`;
-            break
+            case 'heading_2':
+                text_html = convert(block.heading_2);
+                block_html = `<h2>${text_html}</h2>`;
+                break;
 
-        case 'heading_1':
-            text_html = convert(block.heading_1);
-            block_html = `<h1>${text_html}</h1>`;
-            break;
+            case 'heading_3':
+                text_html = convert(block.heading_3);
+                block_html = `<h3>${text_html}</h3>`;
+                break;
 
-        case 'heading_2':
-            text_html = convert(block.heading_2);
-            block_html = `<h2>${text_html}</h2>`;
-            break;
+            case 'table':
+                block_html = `<figure class="wp-block-table"><table><tbody></tbody></table></figure>`
+                break;
 
-        case 'heading_3':
-            text_html = convert(block.heading_3);
-            block_html = `<h3>${text_html}</h3>`;
-            break;
+            case 'table_row':
+                end_tag = "</tbody></table></figure>";
+                if (html.endsWith(end_tag)) {
+                    const length = html.length - end_tag.length;
+                    html = html.substring(0, length);
+                }
 
-        case 'table':
-            block_html = `<figure class="wp-block-table"><table><tbody></tbody></table></figure>`
-            break;
-
-        case 'table_row':
-            end_tag = "</tbody></table></figure>";
-            if (html.endsWith(end_tag)) {
-                const length = html.length - end_tag.length;
-                html = html.substring(0, length);
-            }
-
-            block.table_row.cells.forEach(element => {
-                text_html = convert(element)
-                block_html += `<td>${text_html}</td>`;
-            })
-            block_html = `<tr>${block_html}</tr></tbody></table></figure>`;
-            break;
+                block.table_row.cells.forEach(element => {
+                    text_html = convert(element)
+                    block_html += `<td>${text_html}</td>`;
+                })
+                block_html = `<tr>${block_html}</tr></tbody></table></figure>`;
+                break;
 
 
-        case 'bulleted_list_item':
-            text_html = convert(block.bulleted_list_item);
-            block_html = `<ul><li>${text_html}</li></ul>`;
-            break;
+            case 'bulleted_list_item':
+                text_html = convert(block.bulleted_list_item);
+                block_html = `<ul><li>${text_html}</li></ul>`;
+                break;
 
-        case 'numbered_list_item':
-            text_html = convert(block.numbered_list_item);
-            block_html = `<ol><li>${text_html}</li></ol>`;
-            break;
+            case 'numbered_list_item':
+                text_html = convert(block.numbered_list_item);
+                block_html = `<ol><li>${text_html}</li></ol>`;
+                break;
 
-        case 'toggle':
-            text_html = convert(block.toggle);
-            block_html = `<ul class="is-style-arrow"><li>${text_html}</li></ul>`;
-            break;
+            case 'toggle':
+                text_html = convert(block.toggle);
+                block_html = `<ul class="is-style-arrow"><li>${text_html}</li></ul>`;
+                break;
 
-        case 'quote':
-            text_html = convert(block.quote);
-            block_html = `<blockquote class="wp-block-quote"><p>${text_html}</p></blockquote>`;
-            break;
+            case 'quote':
+                text_html = convert(block.quote);
+                block_html = `<blockquote class="wp-block-quote"><p>${text_html}</p></blockquote>`;
+                break;
 
-        case 'divider':
-            block_html = `<hr class="wp-block-separator has-alpha-channel-opacity"/>`
-            break;
+            case 'divider':
+                block_html = `<hr class="wp-block-separator has-alpha-channel-opacity"/>`
+                break;
 
-        case 'unsupported':
-            block_html = `<p><span style="background-color:#cf2e2e">*** Link page not supported ***</span></p>`
-            break;
+            case 'unsupported':
+                block_html = `<p><span style="background-color:#cf2e2e">*** Link page not supported ***</span></p>`
+                break;
 
-        case 'callout':
-            text_html = convert(block.callout);
-            block_html = `<blockquote class="wp-block-quote"><p>${text_html}</p></blockquote>`;
-            break;
+            case 'callout':
+                text_html = convert(block.callout);
+                block_html = `<blockquote class="wp-block-quote"><p>${text_html}</p></blockquote>`;
+                break;
 
-        case 'child_database':
-            block_html = `<p><span style="background-color:#cf2e2e">*** Database not supported ***</span></p>`
-            break;
+            case 'child_database':
+                block_html = `<p><span style="background-color:#cf2e2e">*** Database not supported ***</span></p>`
+                break;
 
-        case 'image':
-            link = block.image.file.url;
-            block_html = `<p><img class="alignnone size-full" src="${link}" /></p>`;
-            break;
+            case 'image':
+                link = block.image.file.url;
+                block_html = `<p><img class="alignnone size-full" src="${link}" /></p>`;
+                break;
 
-        case 'code':
-            text_html = convert(block.code);
-            block_html = `<pre class="wp-block-code"><code>${text_html}</code></pre>`;
-            break;
+            case 'code':
+                text_html = convert(block.code);
+                block_html = `<pre class="wp-block-code"><code>${text_html}</code></pre>`;
+                break;
 
-        case 'equation':
-            block_html = `<p><span style="background-color:#cf2e2e">*** Equation not supported ***</span></p>`
-            break;
+            case 'equation':
+                block_html = `<p><span style="background-color:#cf2e2e">*** Equation not supported ***</span></p>`
+                break;
 
-        case `video`:
-            block_html = block.video.external.url;
-            block_html = `<p><a href="${block_html}">${block_html}</a></p>`
-            break;
+            case `video`:
+                block_html = block.video.external.url;
+                block_html = `<p><a href="${block_html}">${block_html}</a></p>`
+                break;
 
-        case `bookmark`:
-            block_html = block.bookmark.url;
-            block_html = `<p><a href="${block_html}">${block_html}</a></p>`
-            break;
-    }
-    html += block_html;
-})
+            case `bookmark`:
+                block_html = block.bookmark.url;
+                block_html = `<p><a href="${block_html}">${block_html}</a></p>`
+                break;
+        }
+        html += block_html;
+    })
+    html = html.replace(/<\/ul><ul>/g, '').replace(/<\/ol><ol>/g, '');
+    return html;
+}
 
-html = html.replace(/<\/ul><ul>/g, '').replace(/<\/ol><ol>/g, '');
+result = notion_to_html(items);
 return [{
   json: {
     "Output": html
