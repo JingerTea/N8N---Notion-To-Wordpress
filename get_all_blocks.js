bf644fea03c215e0ec0d79b85bc1e4c7653a6e92
JingerTea <72108315+JingerTea@users.noreply.github.com>
2022-10-09 20:39:54 -0400

    Update get_all_blocks.js
    - Changing modules to Node-fetch as request promising is depreciating
    - Fix minor bugs

--- get_all_blocks.js
+++ get_all_blocks.js
@@ -8,41 +8,41 @@ let new_items = [];
 var count = 1;
 
 do {
-    // Initalize
-    if (count == 1){
-        block_id = items[0].json.page_id;
-    } 
-    // Handle previous Response
-    else {
-        if (tasks[0].has_children == true) {
-            block_id = tasks[0].id;
-            new_items = new_items.concat(tasks[0]);
-            temp_remove = tasks.shift();
-        } else if (tasks[0].has_children == false) {
-            new_items = new_items.concat(tasks[0]);
-            temp_remove = tasks.shift();
-            continue;
+        // Initalize
+        if (count == 1){
+                block_id = items[0].json.page_id;
         }
-    }
+        // Handle previous Response
+        else {
+                if (tasks[0].has_children == true) {
+                        block_id = tasks[0].id;
+                        new_items = new_items.concat(tasks[0]);
+                        temp_remove = tasks.shift();
+                } else if (tasks[0].has_children == false) {
+                        new_items = new_items.concat(tasks[0]);
+                        temp_remove = tasks.shift();
+                        continue;
+                }
+        }
 
-    // Get Children
-    const url = `https://api.notion.com/v1/blocks/${block_id}/children?page_size=100`;
-    const headers = {
-            "Accept": "application/json",
-            "Notion-Version": "2022-06-28",
-            "Authorization": api_key
-    };
+        // Get Children
+        const url = `https://api.notion.com/v1/blocks/${block_id}/children?page_size=100`;
+        const headers = {
+                "Accept": "application/json",
+                "Notion-Version": "2022-06-28",
+                "Authorization": api_key
+        };
 
-    // Append Response
-    const response = await fetch(url, { method: 'GET', headers: headers});
-    const data = await response.json();
-    
-    let temp = [];
-    data.results.forEach(element => {
-        temp = temp.concat(element);
-    })
-    tasks = temp.concat(tasks);
-    count += 1;
+        // Append Response
+        const response = await fetch(url, { method: 'GET', headers: headers});
+        const data = await response.json();
+
+        let temp = [];
+        data.results.forEach(element => {
+                temp = temp.concat(element);
+        })
+        tasks = temp.concat(tasks);
+        count += 1;
 
 } while (tasks.length !== 0);
 
